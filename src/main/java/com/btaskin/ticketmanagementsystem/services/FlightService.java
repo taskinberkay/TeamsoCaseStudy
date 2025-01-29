567f974225ee022a331e9a64c0d9da206024643c
Berkay Kaan Taşkın <taskinberkayp@gmail.com>
2025-01-29 23:52:19 +0300

    plane availability control for flight save/update

--- src/main/java/com/btaskin/ticketmanagementsystem/services/FlightService.java
+++ src/main/java/com/btaskin/ticketmanagementsystem/services/FlightService.java
@@ -1,6 +1,7 @@
 package com.btaskin.ticketmanagementsystem.services;
 
 import com.btaskin.ticketmanagementsystem.domain.Flight;
+import com.btaskin.ticketmanagementsystem.domain.Plane;
 import com.btaskin.ticketmanagementsystem.enums.EnumFlightStatus;
 import com.btaskin.ticketmanagementsystem.repositories.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Service;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(rollbackOn = Exception.class)
@@ -24,7 +28,9 @@ public class FlightService extends BaseService<Flight, Long, FlightRepository> {
     @Override
     public Flight save(Flight flight) {
         Flight savedFlight;
-        // todo: There needs to be a control to check if the selected plane is in any other flight between the flight hours.
+        if (!planeAvailableBetweenDates(Optional.of(flight.getId()), flight.getDepartureTime(), flight.getArrivalTime(), flight.getPlane())) {
+            throw new RuntimeException("Plane is unavailable between flight times!");
+        }
         if (flight.getId() == null) {
             savedFlight = super.save(flight);
             flight.setFlightStatus(EnumFlightStatus.SCHEDULED);
@@ -37,6 +43,20 @@ public class FlightService extends BaseService<Flight, Long, FlightRepository> {
             }
         }
         return savedFlight;
+    }
+
+    private boolean planeAvailableBetweenDates(Optional<Long> id, LocalDateTime departureTime, LocalDateTime arrivalTime, Plane plane) {
+        String hql = "SELECT flight FROM Flight flight WHERE flight.plane = :plane " +
+                     "AND (flight.departureTime < :arrivalTime AND flight.arrivalTime > :departureTime)";
+
+        List<Flight> conflictingFlights = entityManager.createQuery(hql, Flight.class)
+                .setParameter("plane", plane)
+                .setParameter("departureTime", departureTime)
+                .setParameter("arrivalTime", arrivalTime)
+                .getResultList();
+
+        return conflictingFlights.isEmpty() ||
+               (conflictingFlights.size() == 1 && conflictingFlights.get(0).getId().equals(id.orElse(null)));
     }
 
     @Override
